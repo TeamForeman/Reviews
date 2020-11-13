e6494b2f3a18eb4129e19df7b2cd8e6222cf9b4e
Matthew <m_kmorgan@hotmail.com>
2020-11-13 00:21:31 -0700

    faker creates one fake listing

--- database/seeder/fakeData.js
+++ database/seeder/fakeData.js
@@ -41,9 +41,10 @@ var profilePics = [
 
 //LISTING CREATER:
 var createAListing = () => {
-  var bnb = [];
+  var bnb = {};
+
   // CREATES RATINGS
-  var ratings = [];
+  // var ratings = [];
   var r1 = randomNumberBetween(1, 5, true);
   var r2 = randomNumberBetween(1, 5, true);
   var r3 = randomNumberBetween(1, 5, true);
@@ -51,8 +52,7 @@ var createAListing = () => {
   var r5 = randomNumberBetween(1, 5, true);
   var r6 = randomNumberBetween(1, 5, true);
   var av = Math.floor(((r1 + r2 + r3 + r4 + r5 + r6) * 10) / 6) / 10;
-
-  ratings.push({
+  var ratings = {
     average: av,
     cleanliness: r1,
     communication: r2,
@@ -60,11 +60,12 @@ var createAListing = () => {
     accuracy: r4,
     location: r5,
     value: r6
-  });
+  };
 
 
   // CREATES REVIEWS
-  var reviews = {posts: []};
+  var reviews = [];
+  reviews;
   var numberOfReviews = randomNumberBetween(3, 15);
   var pictures = photoShuffle(profilePics);
   while (numberOfReviews > 0) {
@@ -72,7 +73,8 @@ var createAListing = () => {
     var picUrl = pictures[numberOfReviews];
     var date = `${faker.date.month()} ${randomNumberBetween(2008, 2020)}`;
     var paragraph = faker.lorem.paragraphs();
-    reviews.posts.push({
+
+    reviews.push({
       name: userName,
       date: date,
       reviewBody: paragraph,
@@ -80,18 +82,16 @@ var createAListing = () => {
     });
     numberOfReviews--;
   }
-  bnb.push(ratings, reviews);
+
+  bnb.rating = ratings;
+  bnb.reviews = reviews;
   return bnb;
 };
 
 
+console.log(createAListing());
 
-// console.log('ratings?', ratingScore);
-// console.log('ratings?', ratingScore);
-// console.log('Amount of reviews?', faker.date.month());
-// console.log('Amount of reviews?', numberOfReviews);
-// console.log('name: ', userName);
-// console.log('name: ', userName);
 
-var date = `${faker.date.month()} ${randomNumberBetween(2008, 2020)}`;
-console.log(console.log(createAListing()));
+module.exports = {
+
+};
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const db = require('../database/db.js');
 const app = express();
 const port = 3000;
+const FAKE = require('../database/seeder/fakeData.js');
 
 
 
