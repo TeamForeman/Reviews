04f0f40673b11c8a7615e1a03c4b6ca4f479c435
Matthew <m_kmorgan@hotmail.com>
2020-11-13 20:47:47 -0700

    api calls running on page load, just fill the console with listing #1

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+  }
+  componentDidMount () {
+    axios.get('/beartnt/ratings')
+      .then(data => {
+        console.log(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    axios.get('/beartnt/reviews')
+      .then(data => {
+        console.log(data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render () {
