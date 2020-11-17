dd456f76c4bafa01fca40de72379954755e474c9
Matthew <m_kmorgan@hotmail.com>
2020-11-17 01:13:53 -0700

    the ranking bars on the reviews render according to the star rating

--- client/Ratings.jsx
+++ client/Ratings.jsx
@@ -2,22 +2,92 @@ import React from 'react';
 
 const Ratings = (props) => {
 
+  var percentage = (score) => {
+    var styles = {
+      height: '10px',
+      backgroundColor: 'black',
+      borderRadius: '10px'
+    };
+    styles.width = (score / 5 * 100) + '%';
+    console.log('here?', styles);
+    return styles;
+  };
+
   return (
     <div>
       <h3 className='average'>🧸 {props.rate.average}  ({props.numOfReviews} reviews)</h3>
       <div className='ratings-box'>
         <div className='rating'>Cleanliness:</div>
-        <div className='rating'>{props.rate.cleanliness}</div>
+
+        <div className='rating'>
+          <div className='base-layer'>
+            <div style={percentage(props.rate.cleanliness)}></div>
+          </div>
+        </div>
+
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.cleanliness}</small>
+        </div>
+
         <div className='rating'>Accuracy:</div>
-        <div className='rating'>{props.rate.accuracy}</div>
+
+        <div className='rating'>
+          <div className='base-layer'>
+            <div style={percentage(props.rate.accuracy)}></div>
+          </div>
+        </div>
+
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.accuracy}</small>
+        </div>
+
         <div className='rating'>Communication:</div>
-        <div className='rating'>{props.rate.communication}</div>
+
+        <div className='rating'>
+          <div className='base-layer'>
+            <div style={percentage(props.rate.communication)}></div>
+          </div>
+        </div>
+
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.communication}</small>
+        </div>
+
         <div className='rating'>Location:</div>
-        <div className='rating'>{props.rate.location}</div>
+
+        <div className='rating'>
+          <div className='base-layer'>
+            <div style={percentage(props.rate.location)}></div>
+          </div>
+        </div>
+
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.location}</small>
+        </div>
+
         <div className='rating'>Check-in:</div>
-        <div className='rating'>{props.rate.checkin}</div>
+
+        <div className='rating' >
+          <div className='base-layer'>
+            <div style={percentage(props.rate.checkin)}></div>
+          </div>
+        </div>
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.checkin}</small>
+        </div>
+
         <div className='rating'>Value:</div>
-        <div className='rating'>{props.rate.value}</div>
+
+        <div className='rating'>
+          <div className='base-layer'>
+            <div style={percentage(props.rate.value)}></div>
+          </div>
+        </div>
+
+        <div className='rating score'>
+          <small>&nbsp; &nbsp;{props.rate.value}</small>
+        </div>
+
       </div>
     </div>
   );
--- client/SingleReview.jsx
+++ client/SingleReview.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 const SingleReview = (props) => {
   return (
     <div className='review'>
-      <span className='profile-picture'>
-        <a href={props.review.profilePic}>
-          <img alt='profile-pic' src={props.review.profilePic} width='60px' height='60px'></img>
-        </a>
-      </span>
-      <span className='name-date'>
-        <p>{props.review.name}</p>
-        <p>{props.review.date}</p>
-      </span>
+      <div className='pic-name-date'>
+        <span className='profile-picture'>
+          <a href={props.review.profilePic}>
+            <img alt='profile-pic' src={props.review.profilePic} width='60px' height='60px'></img>
+          </a>
+        </span>
+        <span className='name-date'>
+          <p>{props.review.name}</p>
+          <p>{props.review.date}</p>
+        </span>
+      </div>
       <p className='review-body'>{props.review.reviewBody}</p>
     </div>
 
