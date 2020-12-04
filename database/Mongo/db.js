094ab904fa0d5ac16c50f49360b99acad019daad
Fabian Yee <fabianyee952@gmail.com>
2020-12-03 20:34:19 -0800

    get reviews for mongo db

--- database/Mongo/db.js
+++ database/Mongo/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var mongoDB = 'mongodb://localhost/beartnt_reviews';
-
+//beartnt_reviews
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,14 +13,53 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// const userSchema = new mongoose.Schema({
+//   name: String
+// });
+
+// const Kitten = mongoose.model('Kitten', kittySchema);
+
+// Kitten.find(function (err, kittens) {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log(kittens);
+//   db.close();
+// });
+
 var Schema = mongoose.Schema;
 
-var User = new Schema({
-  id: Number,
+var userSchema = new Schema({
   name: String,
   profilePic: String
 });
 
-const MyModel = mongoose.model('ModelName', User);
+var reviewSchema = new Schema({
+  reviewBody: String,
+  date: String,
+  'user_id': Number,
+  'product_id': Number
+});
 
-db.close();
+const user = mongoose.model('user', userSchema);
+const review = mongoose.model('review', reviewSchema);
+
+
+const getReviews = (productId, cb) => {
+  review.find({'product_id': productId}, (err, data) => {
+    if (err) { return console.error(err.stack); }
+    cb(data);
+    db.close();
+  });
+};
+
+getReviews(10999, (data) => {
+  console.log(data);
+});
+
+module.exports = {
+  getReviews,
+};
+
+
+
